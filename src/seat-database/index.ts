aafd99ead3ea871027397ee72627e670b8e6843d
Your Name <you@example.com>
2025-04-16 22:47:22 +0500

    add db name env var

--- src/seat-database/index.ts
+++ src/seat-database/index.ts
@@ -2,6 +2,7 @@ import mariadb from "mariadb";
 
 const pool = mariadb.createPool({
   host: process.env.SEAT_DB_HOST,
+  database: process.env.SEAT_DB_DATABASE,
   user: process.env.SEAT_DB_USER,
   password: process.env.SEAT_DB_PASS,
   port: Number(process.env.SEAT_DB_PORT),
--- src/utils/typings/env.d.ts
+++ src/utils/typings/env.d.ts
@@ -7,6 +7,7 @@ declare global {
       EVE_IMAGE_CDN_BASE_URL: string;
       EVE_CORPORATION_ID: string;
       SEAT_DB_HOST: string;
+      SEAT_DB_DATABASE: string;
       SEAT_DB_USER: string;
       SEAT_DB_PASS: string;
       SEAT_DB_PORT: string;
